6c1a262602671b3fc1484295813f2598a5c0df40
Александр <westerovs@mail.ru>
2022-10-14 21:50:04 +0300

    added onDeleted method

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,16 +8,29 @@ import ItemStatusFilter from '../item-status-filter/item-status-filter.js'
 import './app.css'
 
 export default class App extends React.Component {
-  onDeleted = (id) => {
-    console.log('remove', id)
-  }
   
-  render() {
-    const todoData = [
+  state = {
+    todoData: [
       { label: 'Drink Coffee', important: false, id: 1, done: true },
       { label: 'Make Awesome App', important: true, id: 2, done: false },
       { label: 'Have a lunch', important: false, id: 3, done: false }
     ]
+  }
+  
+  onDeleted = (id) => {
+    this.setState(({todoData}) => {
+      const index = todoData.findIndex(item => item.id === id)
+      
+      const before = todoData.slice(0, index)
+      const after = todoData.slice(index + 1)
+      
+      return {
+        todoData: [...before, ...after]
+      }
+    })
+  }
+  
+  render() {
   
     return (
       <div className="app">
@@ -28,7 +41,7 @@ export default class App extends React.Component {
           <ItemStatusFilter/>
         </div>
       
-        <TodoList onDeleted={(id) => console.log('del', id)} todos={todoData} />
+        <TodoList onDeleted={(id) => this.onDeleted(id)} todos={this.state.todoData} />
       </div>
     )
   }
